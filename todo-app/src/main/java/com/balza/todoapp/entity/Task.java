a082537a14b69e4cfd9c223b48f407e2c628c78b
balzasporify <balzasporify@gmail.com>
2025-09-04 19:52:53 +0300

    refactor: delete @data in task, add .env file for docker compose

--- todo-app/src/main/java/com/balza/todoapp/entity/Task.java
+++ todo-app/src/main/java/com/balza/todoapp/entity/Task.java
@@ -1,27 +1,28 @@
 package com.balza.todoapp.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 import java.time.Instant;
 
 @Entity
 @Table(name = "tasks")
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    Long id;
     @Column(name = "title")
-    private String title;
+    String title;
     @Column(name = "description")
-    private String description;
+    String description;
     @Column(name = "due_date")
-    private Instant dueDate;
+    Instant dueDate;
     @Column(name = "status")
-    private String status;
+    String status;
 }
